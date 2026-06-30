c3a41c9a9c460e4b2b969ad3665fda6f9e505f66
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-06-30 21:24:56 +0530

    Module4_Exe_1

---
+++ Module_4_TDD/alex/src/test/java/com/example/alex/MyFirstTest.java
@@ -0,0 +1,13 @@
+package com.example.alex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void test1() {
+        assertEquals(4, 2 + 2);
+    }
+}
