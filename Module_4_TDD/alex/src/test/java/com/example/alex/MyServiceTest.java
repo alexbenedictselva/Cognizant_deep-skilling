c0dbf4bf43daf418a2a08df9036b6c034e7d41bf
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-07-02 18:07:02 +0530

    Module4_Mandatory_Exe5

--- Module_4_TDD/alex/src/test/java/com/example/alex/MyServiceTest.java
+++ Module_4_TDD/alex/src/test/java/com/example/alex/MyServiceTest.java
@@ -18,4 +18,14 @@ public class MyServiceTest {
 
         assertEquals("Mock Data", result);
     }
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
 }
