8de736bd3a84780dc0e59e1435ad91c6ae8eef75
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-07-06 09:19:28 +0530

    Module5_Exe1

---
+++ Module_5_Spring_Core_and_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("Repository Created");
+    }
+
+    public void display() {
+        System.out.println("Inside Repository");
+    }
+}
---
+++ Module_5_Spring_Core_and_Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("Service Created");
+    }
+
+    public void display() {
+        System.out.println("Inside Service");
+    }
+}
