9fb02784a4f2b18207bf4c292d62835531e9ea92
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-07-23 11:28:19 +0530

    Module7_Exe5

--- Module_7_REST_Spring_Boot_3/alex/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Module_7_REST_Spring_Boot_3/alex/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,11 +1,14 @@
 package com.cognizant.springlearn.controller;
 
 import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +18,9 @@ public class CountryController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
     public List<Country> getAllCountries() {
         LOGGER.info("Start getAllCountries()");
@@ -22,5 +28,13 @@ public class CountryController {
         List<Country> countries = (List<Country>) context.getBean("countryList");
         LOGGER.info("End getAllCountries()");
         return countries;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("Start getCountry()");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("End getCountry()");
+        return country;
     }
 }
