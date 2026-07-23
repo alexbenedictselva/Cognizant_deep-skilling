01934a4fb37258c2813f8b9bf695edca7e7481f8
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-07-23 10:13:20 +0530

    Module7_Exe3

--- Module_7_REST_Spring_Boot_3/alex/src/main/java/com/cognizant/alex/AlexApplication.java
+++ Module_7_REST_Spring_Boot_3/alex/src/main/java/com/cognizant/alex/AlexApplication.java
@@ -1,43 +1,13 @@
 package com.cognizant.alex;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+@SpringBootApplication(scanBasePackages = "com.cognizant")
 public class AlexApplication {
 
     public static void main(String[] args) {
-
-        displayDate();
-
-    }
-
-    public static void displayDate() {
-
-        // Load Spring XML Configuration
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("date-format.xml");
-
-        // Retrieve the bean
-        SimpleDateFormat format =
-                context.getBean("dateFormat", SimpleDateFormat.class);
-
-        try {
-
-            // Parse the String into Date object
-            Date date = format.parse("31/12/2018");
-
-            // Print the Date
-            System.out.println(date);
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-
+        SpringApplication.run(AlexApplication.class, args);
     }
 
 }
